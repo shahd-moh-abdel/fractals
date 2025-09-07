6e017c1e8f97685ae2a55107133b22d981852f7a
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-09-07 22:22:11 +0300

    rainbow

--- fractals.glsl
+++ fractals.glsl
@@ -1,3 +1,5 @@
+#define PI 3.14159
+
 vec3 colorA = vec3(1.0, 0.0, 0.0);
 vec3 colorB = vec3(0.0, 0.0, 1.0);
 
@@ -10,13 +12,14 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
   vec2 uv = fragCoord.xy / iResolution.xy;
   
-  vec3 color = vec3(0.0);
-
-  vec3 pct = vec3(uv.x);
-
+  vec3 color = vec3(0.8, 0.8, 1.0);
+  vec3 pct = vec3(sin(uv.x * PI));
+  
+  //color = mix(colorA, colorB, pct);
+  
   color = mix(color, vec3(1.0, 0.0, 0.0), plot(uv, pct.r));
-  //color = mix(color, vec3(0.0, 1.0, 0.0), plot(uv, pct.g));
-  //color = mix(color, vec3(0.0, 0.0, 1.0), plot(uv, pct.b));
-    
+  color = mix(color, vec3(0.0, 1.0, 0.0), plot(uv, pct.g - 0.1));
+  color = mix(color, vec3(0.0, 0.0, 1.0), plot(uv, pct.b - 0.2));
+   
   fragColor = vec4(color, 1.0);
 }
