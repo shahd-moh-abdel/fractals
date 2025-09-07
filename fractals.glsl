17d86cf42bb9596222d67952233ea388f61937c9
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-09-07 23:44:55 +0300

    border drawing

--- fractals.glsl
+++ fractals.glsl
@@ -8,18 +8,34 @@ float plot (vec2 uv, float pct)
   return smoothstep(pct - 0.01, pct, uv.y) - smoothstep(pct, pct + 0.01, uv.y);
 }
 
+float borders(vec2 uv, float width)
+{
+  float left = step(width, uv.x);
+  float right = step(width, 1.0-uv.x);
+
+  float bottom = step(width, uv.y);
+  float top = step(width, 1.0-uv.y);
+
+  return left * right * bottom * top;
+}
+
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
   vec2 uv = fragCoord.xy / iResolution.xy;
   
   vec3 color = vec3(0.8, 0.8, 1.0);
-  vec3 pct = vec3(sin(uv.x * PI));
+  //vec3 pct = vec3(sin(uv.x * PI));
   
   //color = mix(colorA, colorB, pct);
   
-  color = mix(color, vec3(1.0, 0.0, 0.0), plot(uv, pct.r));
-  color = mix(color, vec3(0.0, 1.0, 0.0), plot(uv, pct.g - 0.1));
-  color = mix(color, vec3(0.0, 0.0, 1.0), plot(uv, pct.b - 0.2));
-   
+  //color = mix(color, vec3(1.0, 0.0, 0.0), plot(uv, pct.r));
+  //color = mix(color, vec3(0.0, 1.0, 0.0), plot(uv, pct.g - 0.1));
+  //color = mix(color, vec3(0.0, 0.0, 1.0), plot(uv, pct.b - 0.2));
+
+  //draing a rectangle
+  float width = 0.1;
+  float border = borders(uv, width);
+
+  color = vec3(border, 0.7, 0.7);
   fragColor = vec4(color, 1.0);
 }
