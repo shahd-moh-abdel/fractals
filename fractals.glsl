ff6ac3307f23bc1fbece9ec3a8519b54459ffab5
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-09-07 18:00:47 +0300

    a line

--- fractals.glsl
+++ fractals.glsl
@@ -1,13 +1,22 @@
-vec3 colorA = vec3(1.0, .0, .0);
-vec3 colorB = vec3(.0, .0, 1.0);
+vec3 colorA = vec3(1.0, 0.0, 0.0);
+vec3 colorB = vec3(0.0, 0.0, 1.0);
+
+float plot (vec2 uv, float pct)
+{
+  return smoothstep(pct - 0.01, pct, uv.y) - smoothstep(pct, pct + 0.01, uv.y);
+}
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-    vec3 red = vec3(0.0);
+  vec2 uv = fragCoord.xy / iResolution.xy;
+  
+  vec3 color = vec3(0.0);
 
-    float pct = abs(sin(iTime));
-   
+  vec3 pct = vec3(uv.x);
+
+  color = mix(color, vec3(1.0, 0.0, 0.0), plot(uv, pct.r));
+  //color = mix(color, vec3(0.0, 1.0, 0.0), plot(uv, pct.g));
+  //color = mix(color, vec3(0.0, 0.0, 1.0), plot(uv, pct.b));
     
-    
-    fragColor = vec4(red,1.0);
+  fragColor = vec4(color, 1.0);
 }
